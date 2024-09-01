33f69726a6d687ee7140aa45d18d3ca0197eb73e
Aashish-Kumar-13 <150321877+Aashish-Kumar-13@users.noreply.github.com>
2024-09-01 17:05:45 +0530

    Loops

---
+++ ch_5_loop_control_instruction.java
@@ -0,0 +1,74 @@
+public class ch_5_loop_control_instruction {
+    public static void main(String[] args) {
+//        1.While Loop
+//        int i=1;
+//        while (i<=10){    //to print 1 to 10
+//            System.out.println(i);
+//            i++;
+//        }
+//        int i = 100;
+//        while(i<=200){
+//            System.out.println(i);
+//            i++;
+//        }
+//        Infinite Loop
+//        while (true){
+//            System.out.println("True");
+//        }
+
+
+//        2.Do While Loop
+//        to print 1 to 10 on do while loop
+//        int i=1;
+//        do {
+//            System.out.println(i);
+//            i++;
+//        }while (i<=10);
+
+//        To print 1 to n number
+//        System.out.println("Enter your number:");
+//        Scanner sc=new Scanner(System.in);
+//        int n=sc.nextInt();
+//        int i=1;
+//        do {
+//            System.out.println(i);
+//            i++;
+//        }while (i<=n);
+
+//        3.For Loop
+//        To print 1 to 10
+//        for (int i=1;i<=10;i++){
+//            System.out.println(i);
+//        }
+
+//        int n=3;
+//        System.out.println("Enter value to print first n odd numbers ");
+//        Scanner sc=new Scanner(System.in);
+//        int n=sc.nextInt();
+// for (int i=1;i<=n;i++){
+//            System.out.println(2*i+1);
+//        }
+
+//          revere order print
+//        int n=1;
+//        for (int i=10;i>=n;i--){
+//            System.out.println(i);
+//        }
+//        Statments
+//        break;
+//        for (int i=10;i>0;i--){
+//        if (i==6){
+//            break;  //complete exit of loop
+//        }
+//            System.out.println(i);
+//        }
+//        Continue;
+//  for (int i=10;i>0;i--){
+//        if (i==6){
+//            continue; //6 in not print only
+//        }
+//            System.out.println(i);
+//        }
+
+    }
+}
